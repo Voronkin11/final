8e03dee9dfc3df01f9dc1526bf8e6a1b6dda24f8
voronkin11 <vvadk11@gmail.com>
2018-10-09 00:47:49 +0300

    Validate project, add marker on maps

--- js/map.js
+++ js/map.js
@@ -1,32 +1,32 @@
 (function() {
 	'use strict'
 
-	/*var map;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          zoom: 8
-        });
-*/
-
 	var mapElement = document.getElementById('map');
 	var place = {
-		lat: 49.070092,
-		lng: 33.405736
+		lat: 49.070551,
+		lng: 33.405282
 	};
 	
 	var map = new google.maps.Map(
 		mapElement, 
 		{
-			zoom: 17, 
+			zoom: 16, 
 			center: place
 		}
 	);
 
-/*	var marker = new google.maps.Marker({
+	var marker = new google.maps.Marker({
 		position: place,
 		map: map,
-		icon: "img/map-marker.png"
-	});*/
- 
+		icon: "img/map-marker.png",
+		title: "IT Place"
+	});
+
+	var info = new google.maps.InfoWindow({
+		content: '<h4>Здесь проходят курсы от Beetroot Academy</h4>'
+	});
+ 	
+ 	marker.addListener('click', function() {
+ 		info.open(mapElement, marker);
+ 	});
 })();
